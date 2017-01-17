810582eb10589932ebfeff5bdc9ec824284c6f15
Drew Hollenback <drewsee26@gmail.com>
2017-01-17 02:40:18 +0000

    CP 9 Address Bloc Menu Cont

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -78,9 +78,38 @@ class MenuController
     end
         
     def search_entries
+        print "Search by name: "
+        name = gets.chomp
+        
+        match = address_book.binary_search(name)
+        system "clear"
+        
+        if match
+            puts match.to_s
+            search_submenu(match)
+        else
+            puts "No match found for #{name}"
+        end
     end
     
     def read_csv
+        print "Enter CSV file to import: "
+        file_name = gets.chomp
+        
+        if file_name.empty?
+            system "clear"
+            puts "No CSV file read"
+            main_menu
+        end
+        
+        begin
+            entry_count = address_book.import_from_csv(file_name).count
+            system "clear"
+            puts "#{entry_count} new entries added from #{file_name}"
+        rescue
+            puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+            read_csv
+        end
     end
     
     def entry_submenu(entry)
@@ -95,7 +124,10 @@ class MenuController
         case selection
             when "n"
             when "d"
+                delete_entry(entry)
             when "e"
+                edit_entry(entry)
+                entry_submenu(entry)
             when "m"
                 system "clear"
                 main_menu
@@ -104,5 +136,28 @@ class MenuController
                 puts "#{selection} is not a valid input"
                 entry_submenu(entry)
         end
+    end
+    
+    def delete_entry(entry)
+        address_book.entries.delete(entry)
+        puts "#{entry.name} has been deleted"
+    end
+    
+    def edit_entry(entry)
+        
+        print "Updated name: "
+        name = gets.chomp
+        print "Updated phone number: "
+        phone_number = gets.chomp
+        print "Updated email: "
+        email = gets.chomp
+        
+        entry.name = name if !name.empty?
+        entry.phone_number = phone_number if !phone_number.empty?
+        entry.email = email if !email.empty?
+        system "clear"
+        
+        puts "Updated entry:"
+        puts entry
     end
 end
